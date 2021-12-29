d9ee5f0e6ae900e7d7691f2613d6e3cd26a4a432
Dmirty12 <96833795+Dmirty12@users.noreply.github.com>
2021-12-29 22:59:46 +0300

    Dll first comit

---
+++ Evaluation/Evaluation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation
+{
+    static public class Evaluation
+    {
+        static public double Sum(double Number1, double Number2)
+        {
+            return (Number1 + Number2);
+        }
+    }
+}
